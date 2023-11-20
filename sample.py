4d6dc34ea76dbcc339d8396252c3c51342c36f2a
Alex Birch <Birch-san@users.noreply.github.com>
2023-11-20 22:23:44 +0000

    add sample.py support for RGB

--- sample.py
+++ sample.py
@@ -14,7 +14,8 @@ from torchvision.utils import save_image
 from diffusion import create_diffusion
 from diffusers.models import AutoencoderKL
 from download import find_model
-from models import DiT_models
+from models import DiT_models, DiTFactory, DiT
+from typing import Optional
 import argparse
 
 
@@ -30,9 +31,16 @@ def main(args):
         assert args.num_classes == 1000
 
     # Load model:
-    latent_size = args.image_size // 8
-    model = DiT_models[args.model](
-        input_size=latent_size,
+    model_factory: DiTFactory = DiT_models[args.model]
+    is_latent: bool = not getattr(model_factory, 'is_rgb_model_factory', False)
+    if is_latent:
+        assert args.image_size % 8 == 0, "Image size must be divisible by 8 (for the VAE encoder)."
+        input_size: int = args.image_size // 8
+    else:
+        input_size: int = args.image_size
+
+    model: DiT = model_factory(
+        input_size=input_size,
         num_classes=args.num_classes
     ).to(device)
     # Auto-download a pre-trained model or load a custom DiT checkpoint from train.py:
@@ -41,19 +49,22 @@ def main(args):
     model.load_state_dict(state_dict)
     model.eval()  # important!
     diffusion = create_diffusion(str(args.num_sampling_steps))
-    vae = AutoencoderKL.from_pretrained(f"stabilityai/sd-vae-ft-{args.vae}").to(device)
+    if is_latent:
+        vae: AutoencoderKL = AutoencoderKL.from_pretrained(f"stabilityai/sd-vae-ft-{args.vae}").to(device)
+    else:
+        vae: Optional[AutoencoderKL] = None
 
     # Labels to condition the model with (feel free to change):
-    class_labels = [207, 360, 387, 974, 88, 979, 417, 279]
+    class_labels = [0, 1, 2, 3, 4, 5, 6, 7] # changed to be <10 so I can test it with artbench
 
     # Create sampling noise:
     n = len(class_labels)
-    z = torch.randn(n, 4, latent_size, latent_size, device=device)
+    z = torch.randn(n, model.in_channels, input_size, input_size, device=device)
     y = torch.tensor(class_labels, device=device)
 
     # Setup classifier-free guidance:
     z = torch.cat([z, z], 0)
-    y_null = torch.tensor([1000] * n, device=device)
+    y_null = torch.tensor([args.num_classes] * n, device=device)
     y = torch.cat([y, y_null], 0)
     model_kwargs = dict(y=y, cfg_scale=args.cfg_scale)
 
@@ -62,7 +73,8 @@ def main(args):
         model.forward_with_cfg, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs, progress=True, device=device
     )
     samples, _ = samples.chunk(2, dim=0)  # Remove null class samples
-    samples = vae.decode(samples / 0.18215).sample
+    if is_latent:
+        samples = vae.decode(samples / 0.18215).sample
 
     # Save and display images:
     save_image(samples, "sample.png", nrow=4, normalize=True, value_range=(-1, 1))
@@ -72,12 +84,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, choices=list(DiT_models.keys()), default="DiT-XL/2")
     parser.add_argument("--vae", type=str, choices=["ema", "mse"], default="mse")
-    parser.add_argument("--image-size", type=int, choices=[256, 512], default=256)
+    parser.add_argument("--image-size", type=int, choices=[128, 256, 512], default=256)
     parser.add_argument("--num-classes", type=int, default=1000)
     parser.add_argument("--cfg-scale", type=float, default=4.0)
     parser.add_argument("--num-sampling-steps", type=int, default=250)
     parser.add_argument("--seed", type=int, default=0)
     parser.add_argument("--ckpt", type=str, default=None,
                         help="Optional path to a DiT checkpoint (default: auto-download a pre-trained DiT-XL/2 model).")
+    parser.add_argument("--mixed-bf16", action='store_true', help="Enables bfloat16 mixed-precision. More intended for testing in low-memory environments.")
     args = parser.parse_args()
     main(args)
